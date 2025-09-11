60a743400b4636c16f85b4652af7651945db579d
Jyarn <58491036+Jyarn@users.noreply.github.com>
2025-09-11 01:54:56 -0400

    create Window class

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,27 @@
+#include <termios.h>
+#include <unistd.h>
+
 #include <iostream>
 #include <thread>
 #include <chrono>
 
-int
-main(int argc, char** argv)
-{
+class Window {
+    public:
+        Window(void);
+        ~Window(void);
+    private:
+        struct termios oldTermConfig;
+};
+
+Window::Window(void) {
+    // save old terminal config
+    tcgetattr(STDOUT_FILENO, &oldTermConfig);
+
+    // enter raw mode
+    struct termios newConfig;
+    cfmakeraw(&newConfig);
+    tcsetattr(STDOUT_FILENO, TCSANOW, &newConfig);
+
     // enable alt buffer
     std::cout << "\x1b[?1049h\n";
 
@@ -13,10 +30,21 @@ main(int argc, char** argv)
 
     // move cursor to home position
     std::cout << "\x1b[H";
+}
+
+Window::~Window(void) {
+    // disable alt buffer
+    std::cout << "\x1b[?1049l\n";
+
+    // exit raw mode
+    tcsetattr(STDOUT_FILENO, TCSANOW, &oldTermConfig);
+}
+
+int
+main(int argc, char** argv)
+{
+    Window window = Window();
 
     std::cout << "\x1b[1;31mHello World!\n";
     std::this_thread::sleep_for (std::chrono::seconds(1));
-
-    // disable alt buffer
-    std::cout << "\x1b[?1049l\n";
 }
